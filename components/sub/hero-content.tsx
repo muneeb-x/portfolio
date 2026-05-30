7ca9f6f3e53075af7997a09f9e3d10ac253e7b9e
muneeb-x <muneebkhurram69@gmail.com>
2026-05-30 16:31:19 +0500

    Match name gradient color to data-driven

--- components/sub/hero-content.tsx
+++ components/sub/hero-content.tsx
@@ -33,7 +33,7 @@ export const HeroContent = () => {
           variants={slideInFromLeft(0.5)}
           className="flex flex-col gap-6 mt-6 text-4xl lg:text-6xl text-bold text-white max-w-[600px] w-auto h-auto"
         >
-          <span className="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-cyan-300">
+          <span className="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
             ABDUL MUNEEB
           </span>
           <span>
@@ -132,7 +132,7 @@ export const HeroContent = () => {
           variants={slideInFromLeft(0.5)}
           className="w-full flex flex-col gap-2 text-3xl text-bold text-white text-center"
         >
-          <span className="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-cyan-300">
+          <span className="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
             ABDUL MUNEEB
           </span>
         </motion.div>
