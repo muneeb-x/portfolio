1d33e6222ae5556e7c55c5c74d04026e20a2a5d4
muneeb-x <muneebkhurram69@gmail.com>
2026-05-30 16:34:35 +0500

    Increase mobile top padding to pt-64

--- components/sub/hero-content.tsx
+++ components/sub/hero-content.tsx
@@ -89,7 +89,7 @@ export const HeroContent = () => {
       </motion.div>
 
       {/* ===== MOBILE LAYOUT ===== */}
-      <div className="md:hidden flex flex-col items-center w-full gap-4 pt-40">
+      <div className="md:hidden flex flex-col items-center w-full gap-4 pt-64">
         {/* Badge */}
         <motion.div
           variants={slideInFromTop}
